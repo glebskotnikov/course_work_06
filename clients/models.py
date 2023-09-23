528749037db4ba8a38943dededbb734c84efddfd
glebskotnikov <skotnikov.nsk@gmail.com>
2023-09-22 19:19:28 -0600

    add mailings and clients apps

--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Client(models.Model):
+    email = models.EmailField(unique=True, verbose_name='почта')
+    full_name = models.CharField(max_length=255, verbose_name='ФИО')
+    comment = models.TextField(verbose_name='комментарий', **NULLABLE)
+
+    def __str__(self):
+        return self.full_name
+
+    class Meta:
+        verbose_name = 'клиент'
+        verbose_name_plural = 'клиенты'
--- mailings/models.py
+++ mailings/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Mailing(models.Model):
+    SEND_FREQUENCY_CHOICES = [
+        ('daily', 'Раз в день'),
+        ('weekly', 'Раз в неделю'),
+        ('monthly', 'Раз в месяц'),
+    ]
+
+    send_time = models.TimeField(verbose_name='время рассылки')
+    send_frequency = models.CharField(max_length=10, choices=SEND_FREQUENCY_CHOICES, verbose_name='периодичность')
+    status = models.CharField(max_length=20, default='создана', verbose_name='статус')
+    start_datetime = models.DateTimeField()
+    end_datetime = models.DateTimeField()
+    clients = models.ManyToManyField('clients.Client', related_name='mailings')
+
+    def __str__(self):
+        return f"{self.get_send_frequency_display()} рассылка в {self.send_time}"
+
+    class Meta:
+        verbose_name = 'рассылка'
+        verbose_name_plural = 'рассылки'
+
+
+class Message(models.Model):
+    mailing = models.ForeignKey('mailings.Mailing', on_delete=models.CASCADE, related_name='messages')
+    subject = models.CharField(max_length=255, verbose_name='тема письма')
+    body = models.TextField(verbose_name='тело письма')
+
+    def __str__(self):
+        return self.subject
+
+    class Meta:
+        verbose_name = 'сообщение'
+        verbose_name_plural = 'сообщения'
